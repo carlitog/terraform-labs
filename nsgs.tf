135736b2705c7a51cb4a4d02be733c75b76ca61e
Carl <carl.gann@gmail.com>
2021-01-15 15:30:54 +1100

    Update: VPN gateway

--- nsgs.tf
+++ nsgs.tf
@@ -4,21 +4,21 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "nsgs" {
     name = "NSGs"
-    location = "var.loc"
-    tags        = "var.tags"
+    location = var.loc
+    tags        = var.tags
 }
 
 resource "azurerm_network_security_group" "resource_group_default" {
    name = "ResourceGroupDefault"
-   resource_group_name  = "azurerm_resource_group.nsgs.name"
-   location             = "azurerm_resource_group.nsgs.location"
-   tags                 = "azurerm_resource_group.nsgs.tags"
+   resource_group_name  = azurerm_resource_group.nsgs.name
+   location             = azurerm_resource_group.nsgs.location
+   tags                 = azurerm_resource_group.nsgs.tags
 }
 
 resource "azurerm_network_security_rule" "AllowSSH" {
     name = "AllowSSH"
-    resource_group_name         = "azurerm_resource_group.nsgs.name"
-    network_security_group_name = "azurerm_network_security_group.resource_group_default.name"
+    resource_group_name         = azurerm_resource_group.nsgs.name
+    network_security_group_name = azurerm_network_security_group.resource_group_default.name
 
     priority                    = 1010
     access                      = "Allow"
@@ -32,8 +32,8 @@ resource "azurerm_network_security_rule" "AllowSSH" {
 
 resource "azurerm_network_security_rule" "AllowHTTP" {
     name = "AllowHTTP"
-    resource_group_name         = "azurerm_resource_group.nsgs.name"
-    network_security_group_name = "azurerm_network_security_group.resource_group_default.name"
+    resource_group_name         = azurerm_resource_group.nsgs.name
+    network_security_group_name = azurerm_network_security_group.resource_group_default.name
 
     priority                    = 1020
     access                      = "Allow"
@@ -47,8 +47,8 @@ resource "azurerm_network_security_rule" "AllowHTTP" {
 
 resource "azurerm_network_security_rule" "AllowHTTPS" {
     name = "AllowHTTPS"
-    resource_group_name         = "azurerm_resource_group.nsgs.name"
-    network_security_group_name = "azurerm_network_security_group.resource_group_default.name"
+    resource_group_name         = azurerm_resource_group.nsgs.name
+    network_security_group_name = azurerm_network_security_group.resource_group_default.name
 
     priority                    = 1021
     access                      = "Allow"
@@ -62,8 +62,8 @@ resource "azurerm_network_security_rule" "AllowHTTPS" {
 
 resource "azurerm_network_security_rule" "AllowSQLServer" {
     name = "AllowSQLServer"
-    resource_group_name         = "azurerm_resource_group.nsgs.name"
-    network_security_group_name = "azurerm_network_security_group.resource_group_default.name"
+    resource_group_name         = azurerm_resource_group.nsgs.name
+    network_security_group_name = azurerm_network_security_group.resource_group_default.name
 
     priority                    = 1030
     access                      = "Allow"
@@ -75,11 +75,27 @@ resource "azurerm_network_security_rule" "AllowSQLServer" {
     source_address_prefix       = "*"
 }
 
+resource "azurerm_network_security_rule" "AllowRDP" {
+    name = "AllowRDP"
+    resource_group_name         = azurerm_resource_group.nsgs.name
+    network_security_group_name = azurerm_network_security_group.resource_group_default.name
+
+    priority                    = 1040
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 3389
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+}
+
+
 resource "azurerm_network_security_group" "nic_ubuntu" {
    name = "NIC_Ubuntu"
-   resource_group_name  = "azurerm_resource_group.nsgs.name"
-   location             = "azurerm_resource_group.nsgs.location"
-   tags                 = "azurerm_resource_group.nsgs.tags"
+   resource_group_name  = azurerm_resource_group.nsgs.name
+   location             = azurerm_resource_group.nsgs.location
+   tags                 = azurerm_resource_group.nsgs.tags
 
     security_rule {
         name                       = "SSH"
@@ -89,6 +105,25 @@ resource "azurerm_network_security_group" "nic_ubuntu" {
         protocol                   = "Tcp"
         source_port_range          = "*"
         destination_port_range     = 22
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_security_group" "nic_windows" {
+   name = "NIC_Windows"
+   resource_group_name  = azurerm_resource_group.nsgs.name
+   location             = azurerm_resource_group.nsgs.location
+   tags                 = azurerm_resource_group.nsgs.tags
+
+    security_rule {
+        name                       = "AllowRDP"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = 3389
         source_address_prefix      = "*"
         destination_address_prefix = "*"
   }
